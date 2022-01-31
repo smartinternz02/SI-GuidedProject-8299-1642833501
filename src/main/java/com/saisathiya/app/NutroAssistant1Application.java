4de9141e3e6ba607f95c6c66b9c5990aef568962
saisathiyar <98479320+saisathiyar@users.noreply.github.com>
2022-01-31 15:34:21 +0530

    Add files via upload

---
+++ src/main/java/com/saisathiya/app/NutroAssistant1Application.java
@@ -0,0 +1,13 @@
+package com.saisathiya.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutroAssistant1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutroAssistant1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/saisathiya/app/repository/InputRepo.java
@@ -0,0 +1,8 @@
+package com.saisathiya.app.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.saisathiya.app.model.Input;
+
+public interface InputRepo extends CrudRepository<Input,String>{
+}
